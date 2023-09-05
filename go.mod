58dfdb4b1998e8818b90b0ccdee6ae4ec395d877
Adarsh Kumar <contactadarshk@gmail.com>
2023-09-05 20:52:00 +0530

    add rate limiting

--- go.mod
+++ go.mod
@@ -11,12 +11,14 @@ require (
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/lib/pq v1.2.0
 	github.com/rs/zerolog v1.30.0
+	github.com/ulule/limiter/v3 v3.11.2
 	golang.org/x/crypto v0.12.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
 	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 )
--- pkg/app/routes.go
+++ pkg/app/routes.go
@@ -1,15 +1,23 @@
 package app
 
 import (
+	"net/http"
+
 	"github.com/akxcix/passport/pkg/handlers"
 	authHandlers "github.com/akxcix/passport/pkg/handlers/auth"
 	"github.com/akxcix/passport/pkg/services/auth"
+	"github.com/rs/zerolog/log"
+	limiter "github.com/ulule/limiter/v3"
+	mhttp "github.com/ulule/limiter/v3/drivers/middleware/stdlib"
+	"github.com/ulule/limiter/v3/drivers/store/memory"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 )
 
 func createRoutes(authService *auth.Service) *chi.Mux {
+	rateLimiter := NewRateLimiter()
+	limiterMiddleware := rateLimiter.rateLimitMiddleware()
 	r := chi.NewRouter()
 
 	// global middlewares
@@ -21,6 +29,7 @@ func createRoutes(authService *auth.Service) *chi.Mux {
 		AllowCredentials: false,
 		MaxAge:           300,
 	}))
+	r.Use(limiterMiddleware)
 	r.Use(handlers.LogRequest)
 
 	// general routes
@@ -33,4 +42,35 @@ func createRoutes(authService *auth.Service) *chi.Mux {
 	r.Post("/users/update", authHandlers.AuthMiddleware(authHandlers.UpdateUser))
 
 	return r
+}
+
+// rate limiter -----------------------------------------------------------------------------------
+type RateLimiter struct {
+	store limiter.Store
+	rate  limiter.Rate
+}
+
+func NewRateLimiter() *RateLimiter {
+	rate, err := limiter.NewRateFromFormatted("500-M")
+	if err != nil {
+		log.Fatal().Err(err).Msg("unable to initialise limiter")
+	}
+
+	store := memory.NewStore()
+
+	limiter := RateLimiter{
+		store: store,
+		rate:  rate,
+	}
+	return &limiter
+}
+
+func (l *RateLimiter) rateLimitMiddleware() func(h http.Handler) http.Handler {
+	middleware := mhttp.NewMiddleware(limiter.New(
+		l.store,
+		l.rate,
+		limiter.WithTrustForwardHeader(true),
+	))
+
+	return middleware.Handler
 }
